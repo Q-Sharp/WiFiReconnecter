2ee21068b5c4585d8790080857ed7da0947c5007
QoOdrink3r <qoodrink3r@outlook.de>
2018-05-21 19:07:33 +0200

    Init

--- WiFiReconnecter/Program.cs
+++ WiFiReconnecter/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace WiFiReconnecter
 {
@@ -10,6 +14,51 @@ namespace WiFiReconnecter
     {
         static void Main(string[] args)
         {
+            Task.Run(() => new WiFiReconnecter());
+
+            while(true);
+
+            //while(true)
+            //{
+            //    if(!PingHost("8.8.8.8") && !PingHost("8.8.8.8") && !PingHost("8.8.8.8"))
+            //    {
+            //        //var cmd = $"wlan connect profile=\"{}\" ssid=\"{}\"";
+            //        var proc = new Process();
+
+            //        proc.StartInfo.FileName = "netsh.exe";
+            //        proc.StartInfo.Arguments = cmd;
+            //        proc.StartInfo.UseShellExecute = false;
+            //        proc.StartInfo.RedirectStandardOutput = true;
+            //        proc.Start();
+
+            //        Console.WriteLine(proc.StandardOutput.ReadToEnd());
+            //    }
+
+            //    var oT = TimeSpan.FromMinutes(5);
+
+            //    Console.WriteLine($"Connected, trying again in {oT.ToString()}");
+
+            //    // Wait 5 minutes
+            //    Thread.Sleep(oT);
+            //}
         }
+
+        //public static bool PingHost(string nameOrAddress)
+        //{
+        //    var pingable = false;
+        //    var pinger = new Ping();
+
+        //    try
+        //    {
+        //        var reply = pinger.Send(nameOrAddress);
+        //        pingable = reply.Status == IPStatus.Success;
+        //    }
+        //    catch(PingException)
+        //    {
+        //        // ignore
+        //    }
+
+        //    return pingable;
+        //}
     }
 }
---
+++ WiFiReconnecter/WiFiReconnecter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using SimpleWifi;
+
+namespace WiFiReconnecter
+{
+    public class WiFiReconnecter
+    {
+        private List<AccessPoint> oAPs;
+        private AccessPoint oAP;
+
+        public WiFiReconnecter(Wifi oWiFi = null)
+        {
+            Console.WriteLine("WiFi Reconnecter service running...");
+            WiFi = oWiFi ?? new Wifi();
+            oAPs = WiFi?.GetAccessPoints();
+            oAP = oAPs.Where(x => x.IsConnected)?.FirstOrDefault();
+
+            if(oAP == null && CheckForProblems())
+                return;
+
+            WiFi.ConnectionStatusChanged += WiFi_ConnectionStatusChanged;
+            Console.WriteLine("WiFi is connected!");
+        }
+
+        public Wifi WiFi { get; set; }
+
+        private void WiFi_ConnectionStatusChanged(object oSender, WifiStatusEventArgs oArgs)
+        {
+            if(oArgs.NewStatus == WifiStatus.Disconnected)
+                Reconnect();
+        }
+
+        private void Reconnect(TimeSpan? WaitTime = null)
+        {
+            if(oAP != null)
+            {
+                var r = new AuthRequest(oAP);
+
+                if(WaitTime.HasValue)
+                    Thread.Sleep(WaitTime.Value);
+
+                oAP.ConnectAsync(r, false, x =>
+                {
+                    if(!x)
+                    {
+                        Console.WriteLine("Reconnection problem! Trying again in 1 minute...");
+                        Reconnect(TimeSpan.FromMinutes(1));
+                    }
+                    else
+                        Console.WriteLine("reconnected");
+                }); 
+            }
+        }
+
+        private bool CheckForProblems()
+        {
+            oAP = oAPs.Where(x => x.HasProfile).FirstOrDefault();
+
+            if(oAP == null)
+            {
+                Console.WriteLine("Not connected to a wifi access point and can't connect to one. Service stopped.");
+                return true;
+            }
+
+            Reconnect();
+            return false;
+        }
+    }
+}
